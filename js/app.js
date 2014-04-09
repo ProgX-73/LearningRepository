45d0c7176463f130bd0a53d880f583fc9a5e8d48
Fred <progx73@gmail.com>
2014-04-09 16:44:18 +0200

    Update

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('myAppCtrl', function ($scope) {
+myApp.controller('myAppCtrl', function ($scope,$http) {
         
         $scope.helloWorld = "Salut le monde";
-        $scope.data;
-    
+        $scope.data = "";
+     
         $scope.resetBtn = function (btn) {
             $scope.helloWorld = "Reset Clicked";
         }
         
-        $scope.getDataBtn = function (btn,$http) {
-           $http.get('https://api.github.com/legacy/repos/search/LearningRepository')
-        .success(function(datas) {
-            $scope.data = datas.repositories;
-        });
+        $scope.getDataBtn = function (btn) {
+            
+              $http.get('http://127.0.0.1:54044/LearningRepository/js/data.json')
+                .success(function(datas) {$scope.data = datas;});
+        }
+         
 });
