46ae0f82234bf67bb6881b491506a77f8a9a4441
Fred <progx73@gmail.com>
2014-04-09 16:07:46 +0200

    Ajout data json

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('myAppCtrl', function ($scope) {
-    $scope.helloWorld = "Salut le monde"; 
-    
-    $scope.resetBtn = function(btn){
         
-        $scope.helloWorld = "Reset Clicked";
-    }
+        $scope.helloWorld = "Salut le monde";
+        $scope.data;
+    
+        $scope.resetBtn = function (btn) {
+            $scope.helloWorld = "Reset Clicked";
+        }
+        
+        $scope.getDataBtn = function (btn,$http) {
+           $http.get('https://api.github.com/legacy/repos/search/LearningRepository')
+        .success(function(datas) {
+            $scope.data = datas.repositories;
+        });
 });
