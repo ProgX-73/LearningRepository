277494781b5d28a9a85f6b546035e837ac0c913d
Fred <progx73@gmail.com>
2014-04-09 16:56:19 +0200

    Corrections datas

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var myApp = angular.module('myApp', []);
 myApp.controller('myAppCtrl', function ($scope,$http) {
         
         $scope.helloWorld = "Salut le monde";
-        $scope.data = "";
+        $scope.data = [];
      
         $scope.resetBtn = function (btn) {
             $scope.helloWorld = "Reset Clicked";
